94fa85b70f0d580f00d52049b04ddda4e866929e
shraddhashekhar7 <shekhar7@buffalo.edu>
2025-01-04 16:21:46 -0500

    Completed Task5-CRUD operations and postman screenshots

--- LoginSystem/Loginify/urls.py
+++ LoginSystem/Loginify/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.hello_world, name="hello_world"),
     path("signup/", views.sign_up, name="sign_up"),
     path("login/", views.login, name="login"),
+    path("all_data/", views.all_data, name="all_data"),
+    path("single-user-data/<str:email>/",views.single_user_data, name="single_user_data"),
 ]
--- LoginSystem/Loginify/views.py
+++ LoginSystem/Loginify/views.py
@@ -4,6 +4,9 @@ from .forms import SignupForm, LoginForm
 from .models import UserDetails
 from django.contrib import messages
 from django.contrib.auth.hashers import make_password, check_password
+import json
+from .serializer import UserSerializer
+from django.views.decorators.csrf import csrf_exempt
 
 def hello_world(request):
     return HttpResponse("Hello, world!")
@@ -46,3 +49,78 @@ def login(request):
         form = LoginForm()
     
     return render(request, "Loginify/login.html", {"form":form})
+
+@csrf_exempt
+def all_data(request):
+    if request.method == "GET":
+        try:
+            alldata = UserDetails.objects.all()  
+            serialized_data = UserSerializer(alldata, many=True)  
+            return JsonResponse(serialized_data.data, safe=False)
+        except Exception as e:
+            return JsonResponse({"error":str(e)}, status=500)
+        
+    if request.method == "POST":
+        try:
+            data = json.loads(request.body) 
+            serialized_data = UserSerializer(data = data) 
+            if serialized_data.is_valid():
+                serialized_data.save()
+                return JsonResponse({"message":"Data saved successfully!"},status=201)
+            else:
+                return JsonResponse(serialized_data.errors, status=400)
+        except Exception as e:
+            return JsonResponse({"error":str(e)}, status=500)
+        
+@csrf_exempt        
+def single_user_data(request, email):
+    if request.method == "GET":
+        try:
+            userdata = UserDetails.objects.get(email=email) 
+            serialized_data = UserSerializer(userdata) 
+            return JsonResponse({
+                "data":serialized_data.data}, status=200)
+        except UserDetails.DoesNotExist:
+            return JsonResponse({"error":"User not found"},status=404)
+        except Exception as e:
+            return JsonResponse({"error":str(e)}, status=500)
+        
+    if request.method == "PUT":
+        try:
+            user_data = json.loads(request.body)
+            user = UserDetails.objects.get(email=email)
+            serialized_data = UserSerializer(user, data=user_data)
+            if serialized_data.is_valid():
+                serialized_data.save()
+                return JsonResponse({"message":"Data updated successfully"}, status=200)
+            else:
+                return JsonResponse(serialized_data.errors, status=400)
+        except UserDetails.DoesNotExist:
+            return JsonResponse({"error":"User not found"},status=404)
+        except Exception as e:
+            return JsonResponse({"error":str(e)}, status=500)
+    
+    if request.method == "DELETE":
+        try:
+            user = UserDetails.objects.get(email=email)
+            user.delete()
+            return JsonResponse({"message":"User deleted successfully"},status=200)
+        except UserDetails.DoesNotExist:
+            return JsonResponse({"error":"User not found"},status=404)
+        except Exception as e:
+            return JsonResponse({"error":str(e)}, status=500)
+        
+    if request.method == "PATCH":
+        try:
+            user_data = json.loads(request.body)
+            user = UserDetails.objects.get(email=email)
+            serialized_data = UserSerializer(user, data=user_data, partial=True)
+            if serialized_data.is_valid():
+                serialized_data.save()
+                return JsonResponse({"message":"Data patched successfully"}, status=200)
+            else:
+                return JsonResponse(serialized_data.errors, status=400)
+        except UserDetails.DoesNotExist:
+            return JsonResponse({"error":"User not found"},status=404)
+        except Exception as e:
+            return JsonResponse({"error":str(e)}, status=500)
